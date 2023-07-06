3278a3b32faad3c25736098279055e397402eda9
Peter GORGY <petergorgy@gmail.com>
2023-07-06 11:18:06 +0300

    Add css.

--- app.js
+++ app.js
@@ -4,30 +4,33 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
+let newListOfItems = ["Item 001", "Item 002"];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-const weekday = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
 
 app.get("/", (req, res) => {
-  var today = new Date();
-  var currentDay = today.getDate();
-  var day = weekday[currentDay];
-
-  console.log("weekday: " + weekday);
-  console.log("currentDay: " + currentDay);
-  console.log("weekday[currentDay]: " + weekday[currentDay]);
-
-  res.render("list", { kindOfDay: day });
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  let today = new Date();
+  let currentDate = today.toLocaleDateString("en-US", options);
+  console.log(currentDate); // Saturday, September 17, 2016
+  console.log(newListOfItems);
+  res.render("list", {kindOfDay: currentDate, listOfItems: newListOfItems});
 });
+
+app.post("/", (req, res) => {
+  console.log("newListOfItems[]:" + newListOfItems);
+  console.log("req.body.newItem: " + req.body.newItem);
+  newListOfItems.push(req.body.newItem);
+  console.log("newListOfItems[]:" + newListOfItems);
+  res.redirect("/");
+})
 
 app.listen(port, () => {
   console.log("Server started at port number: " + port + "!");
