49abb1a9a4e2302141d4e11dc32902990a8ed605
Peter GORGY <petergorgy@gmail.com>
2023-07-06 12:24:55 +0300

    Add a new work list, modify the redirections by using variable.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 let newListOfItems = ["Item 001", "Item 002"];
+let workListOfItems = [];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -21,7 +22,7 @@ app.get("/", (req, res) => {
   let currentDate = today.toLocaleDateString("en-US", options);
   console.log(currentDate); // Saturday, September 17, 2016
   console.log(newListOfItems);
-  res.render("list", {kindOfDay: currentDate, listOfItems: newListOfItems});
+  res.render("list", {redirectionPage: "/", listTitle: currentDate, listOfItems: newListOfItems});
 });
 
 app.post("/", (req, res) => {
@@ -30,6 +31,18 @@ app.post("/", (req, res) => {
   newListOfItems.push(req.body.newItem);
   console.log("newListOfItems[]:" + newListOfItems);
   res.redirect("/");
+})
+
+app.get("/work", (req, res)=> {
+  res.render("list", {redirectionPage: "/work", listTitle: "Work List", listOfItems: workListOfItems});
+});
+
+app.post("/work", (req, res) => {
+  console.log("workListOfItems[]:" + workListOfItems);
+  console.log("req.body.newItem: " + req.body.newItem);
+  workListOfItems.push(req.body.newItem);
+  console.log("workListOfItems[]:" + workListOfItems);
+  res.redirect("/work");
 })
 
 app.listen(port, () => {
